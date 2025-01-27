2acc9ba0be54bdb3ca6a614bbcca98f227dbf548
emir <reostraemir@gmail.com>
2025-01-27 19:57:19 +0300

    feat: social accounts

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,10 +6,8 @@ import Link from 'next/link'
 
 function Footer() {
   const socialLinks = [
-    { icon: <Github className="w-5 h-5" />, href: 'https://github.com' },
-    { icon: <Twitter className="w-5 h-5" />, href: 'https://twitter.com' },
-    { icon: <Instagram className="w-5 h-5" />, href: 'https://instagram.com' },
-    { icon: <Linkedin className="w-5 h-5" />, href: 'https://linkedin.com' }
+    { icon: <Instagram className="w-5 h-5" />, href: 'https://instagram.com/witharc.co' },
+    { icon: <Linkedin className="w-5 h-5" />, href: 'https://linkedin.com/company/witharcstudio' }
   ]
 
   return (

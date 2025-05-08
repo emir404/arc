043c3250791516cc727bf8e9c3c4ece9928d1fa9
emir <reostraemir@gmail.com>
2025-05-09 00:21:33 +0300

    fix: prices

--- src/components/ui/sections/pricing.tsx
+++ src/components/ui/sections/pricing.tsx
@@ -83,7 +83,7 @@ function Pricing() {
               </p>
             </div>
             <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>
-              $1199
+              $1397
             </p>
             <div className='py-4 flex flex-col gap-4'>
               <div className='flex gap-3 items-center'>
@@ -156,9 +156,9 @@ function Pricing() {
             </div>
               {
                 mode === "monthly" ? (
-                  <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>$1600 <span className='text-lg text-gray-400 tracking-normal font-normal'>/per month</span></p>
+                  <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>$1800 <span className='text-lg text-gray-400 tracking-normal font-normal'>/per month</span></p>
                 ) : (
-                  <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>$15500 <span className='text-lg text-gray-400 tracking-normal font-normal'>/per year</span></p>
+                  <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>$18000 <span className='text-lg text-gray-400 tracking-normal font-normal'>/per year</span></p>
                 )
               }
             <div className='py-4 flex flex-col gap-4'>
@@ -211,44 +211,6 @@ function Pricing() {
             </Button>
             </Link>
           </div>
-        </div>
-        <div className='w-full h-full flex flex-col md:flex-row p-8  rounded-2xl border border-gray-800/10 gap-12 pricing-cards'>
-            <div className='flex flex-col gap-1'>
-            <p className='tracking-tight font-semibold text-xl text-gray-600'>
-                Redesign
-              </p>
-              <p className='tracking-tight text-lg text-gray-500 max-w-sm'>
-                Best for founders who need a fast design turnaround.
-              </p>
-              <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>
-              $499
-              </p>
-            </div>
-            <div className='flex flex-col gap-3 w-full h-full'>
-              <div className='flex gap-3 items-center'>
-                <FaCircleCheck size={20} className='text-blue-500'/>
-                <p className='text-xl text-gray-600'>
-                  Any page you want (landing, dashboard etc.)
-                </p>
-              </div>
-              <div className='flex gap-3 items-center'>
-                <FaCircleCheck size={20} className='text-blue-500'/>
-                <p className='text-xl text-gray-600'>
-                  48-hour delivery
-                </p>
-              </div>
-              <div className='flex gap-3 items-center'>
-                <FaCircleCheck size={20} className='text-blue-500'/>
-                <p className='text-xl text-gray-600'>
-                  Figma file
-                </p>
-              </div>
-              <Link href={'https://buy.polar.sh/polar_cl_3cb9zz7NQr886SrIcxNEmouAp3bLVvRecukf92q4V2E'} target='_blank' className='w-full mt-auto pt-4'>
-                <Button className='w-full'>
-                  Get Your Redesign
-                </Button>
-              </Link>
-            </div>
         </div>
       </div>
     </div>

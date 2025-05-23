dec6fdb04c342df6dd57ed32805bf6b070d80462
emir <reostraemir@gmail.com>
2025-05-24 00:53:10 +0300

    fix

--- src/components/ui/sections/pricing.tsx
+++ src/components/ui/sections/pricing.tsx
@@ -129,7 +129,7 @@ function Pricing() {
                 </p>
               </div>
             </div>
-            <Link href={'https://buy.polar.sh/polar_cl_6EMNsRORjSA73WXAnEJYtgjvAkKOKB1pcnMYL1bfcTN'} target='_blank' className='w-full mt-auto'>
+            <Link href={'https://t.me/emirthedev'} target='_blank' className='w-full mt-auto'>
               <Button className='w-full'>
                 Get Your Landing Page
               </Button>
@@ -156,9 +156,9 @@ function Pricing() {
             </div>
               {
                 mode === "monthly" ? (
-                  <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>$2495 <span className='text-lg text-gray-400 tracking-normal font-normal'>/per month</span></p>
+                  <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>$2195 <span className='text-lg text-gray-400 tracking-normal font-normal'>/per month</span></p>
                 ) : (
-                  <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>$24950 <span className='text-lg text-gray-400 tracking-normal font-normal'>/per year</span></p>
+                  <p className='font-semibold text-6xl my-2 text-gray-700 tracking-tighter pt-2'>$21950 <span className='text-lg text-gray-400 tracking-normal font-normal'>/per year</span></p>
                 )
               }
             <div className='py-4 flex flex-col gap-4'>
@@ -205,9 +205,9 @@ function Pricing() {
                 </p>
               </div>
             </div>
-            <Link href={mode === "monthly" ? 'https://buy.polar.sh/polar_cl_DXTCbLKmdtW8MXkGtqw7BaX2UriQkAumCehTC2jJfBz' : "https://buy.polar.sh/polar_cl_qrcVLyuvDphP2sHCW24X4MutRs2doGwmzXWIC03r7lD"} target='_blank' className='w-full mt-auto'>
+            <Link href={"https://t.me/emirthedev"} target='_blank' className='w-full mt-auto'>
             <Button className='w-full bg-blue-500 font-semibold'>
-                Get Started
+                Let's Chat <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" width="48" height="48" preserveAspectRatio="xMidYMid"><defs><linearGradient id="a" x1="50%" x2="50%" y1="0%" y2="100%"><stop offset="0%" stopColor="#fff"/><stop offset="100%" stopColor="#fff"/></linearGradient></defs><path fill="url(#a)" d="M128 0C94.06 0 61.48 13.494 37.5 37.49A128.038 128.038 0 0 0 0 128c0 33.934 13.5 66.514 37.5 90.51C61.48 242.506 94.06 256 128 256s66.52-13.494 90.5-37.49c24-23.996 37.5-56.576 37.5-90.51 0-33.934-13.5-66.514-37.5-90.51C194.52 13.494 161.94 0 128 0Z"/><path fill="#000" d="M57.94 126.648c37.32-16.256 62.2-26.974 74.64-32.152 35.56-14.786 42.94-17.354 47.76-17.441 1.06-.017 3.42.245 4.96 1.49 1.28 1.05 1.64 2.47 1.82 3.467.16.996.38 3.266.2 5.038-1.92 20.24-10.26 69.356-14.5 92.026-1.78 9.592-5.32 12.808-8.74 13.122-7.44.684-13.08-4.912-20.28-9.63-11.26-7.386-17.62-11.982-28.56-19.188-12.64-8.328-4.44-12.906 2.76-20.386 1.88-1.958 34.64-31.748 35.26-34.45.08-.338.16-1.598-.6-2.262-.74-.666-1.84-.438-2.64-.258-1.14.256-19.12 12.152-54 35.686-5.1 3.508-9.72 5.218-13.88 5.128-4.56-.098-13.36-2.584-19.9-4.708-8-2.606-14.38-3.984-13.82-8.41.28-2.304 3.46-4.662 9.52-7.072Z"/></svg>
             </Button>
             </Link>
           </div>

b5543a52cb3d60322facee6daf4994133b717ac1
emir <95132095+emir404@users.noreply.github.com>
2025-11-21 23:15:04 +0300

    Update price from $4995 to $4395

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -229,7 +229,7 @@ function Home() {
             </p>
             </div>
             <p className='text-6xl font-medium text-[#fefefe] tracking-[-0.02em]'>
-            $4995/mo
+            $4395/mo
             </p>
             </div>
             <div className='flex flex-col gap-4'>
